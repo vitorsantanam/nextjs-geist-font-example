6304ccc3380e56be181f210b480fa6e5fd176639
Vitor Santana <130749055+vitorsantanam@users.noreply.github.com>
2025-05-16 16:14:28 -0300

    Add .next/static/chunks/src_app_page_tsx_a1f9e366._.js 1747422878035

---
+++ .next/static/chunks/src_app_page_tsx_a1f9e366._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_63ce3389._.js",
+  "static/chunks/src_2badb939._.js"
+],
+    source: "dynamic"
+});
